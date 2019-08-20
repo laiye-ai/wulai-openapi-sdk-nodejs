7803d16eb4185268a66092a0393868ef2f1333c3
jinchaofs <jinchaofs@gmail.com>
2019-08-20 17:32:19 +0800

    feature: refactor client class and remove cmReq module

---
+++ demo.js
@@ -0,0 +1,20 @@
+"use strict";
+const WuLaiClient = require("./index");
+
+const client = new WuLaiClient({
+  pubkey: "eIKUn1ftuqREgJjHUWvEZpZntmlUPQB2007e4a782c3c44c410",
+  secret: "8B2pRLvpYPM3txcuobRo"
+});
+client
+  .getBotResponse({
+    msg_body: {
+      text: {
+        content: "谢谢"
+      }
+    },
+    user_id: "sdjkfhaksdfjkajsjfdkajsdkf",
+    extra: "dfdsfdfsdfs"
+  })
+  .then(res => {
+    console.log(res);
+  });
--- lib/client.d.ts
+++ lib/client.d.ts
@@ -6,68 +6,83 @@ declare class Client {
     avatar_url?: string;
     user_id: string;
   }) => Promise<any>;
-  userAttributeCreate: (data: {
-    user_attribute_user_attribute_value: [
-      {
-        user_attribute: {
-          id: string;
+  userAttributeCreate: (
+    data: {
+      user_attribute_user_attribute_value: [
+        {
+          user_attribute: {
+            id: string;
+          };
+          user_attribute_value: {
+            name: string;
+          };
+        }
+      ];
+      user_id: string;
+    }
+  ) => Promise<any>;
+  userAttributeList: (
+    data: {
+      filter: {
+        use_in_user_attribute_group: boolean;
+      };
+      page: number;
+      page_size: number;
+    }
+  ) => Promise<any>;
+  getHistoryRecord: (
+    data: {
+      direction: "BACKWARD" | "FORWARD";
+      msg_id: string;
+      user_id: string;
+      num: number;
+    }
+  ) => Promise<any>;
+  getBotResponse: (
+    data: {
+      msg_body: {
+        text: {
+          content: string;
         };
-        user_attribute_value: {
-          name: string;
+      };
+      user_id: string;
+      extra: string;
+    }
+  ) => Promise<any>;
+  getKeywordBotResponse: (
+    data: {
+      msg_body: {
+        text: {
+          content: string;
         };
-      }
-    ];
-    user_id: string;
-  }) => Promise<any>;
-  userAttributeList: (data: {
-    filter: {
-      use_in_user_attribute_group: boolean;
-    };
-    page: number;
-    page_size: number;
-  }) => Promise<any>;
-  getHistoryRecord: (data: {
-    direction: "BACKWARD" | "FORWARD";
-    msg_id: string;
-    user_id: string;
-    num: number;
-  }) => Promise<any>;
-  getBotResponse: (data: {
-    msg_body: {
-      text: {
-        content: string;
       };
-    };
-    user_id: string;
-    extra: string;
-  }) => Promise<any>;
-  getKeywordBotResponse: (data: {
-    msg_body: {
-      text: {
-        content: string;
+      user_id: string;
+      extra: string;
+    }
+  ) => Promise<any>;
+  getTaskBotResponse: (
+    data: {
+      msg_body: {
+        text: {
+          content: string;
+        };
       };
-    };
-    user_id: string;
-    extra: string;
-  }) => Promise<any>;
-  getTaskBotResponse: (data: {
-    msg_body: {
-      text: {
-        content: string;
+      user_id: string;
+      extra: string;
+    }
+  ) => Promise<any>;
+  getQABotResponse: (
+    data: {
+      msg_body: {
+        text: {
+          content: string;
+        };
       };
-    };
-    user_id: string;
-    extra: string;
-  }) => Promise<any>;
-  getQABotResponse: (data: {
-    msg_body: {
-      text: {
-        content: string;
-      };
-    };
-    user_id: string;
-    extra: string;
-  }) => Promise<any>;
+      user_id: string;
+      extra: string;
+    }
+  ) => Promise<any>;
+  request: (action: string, params: Object, opts: Object) => Promise<any>;
 }
 declare namespace Client {
   export interface Config {
